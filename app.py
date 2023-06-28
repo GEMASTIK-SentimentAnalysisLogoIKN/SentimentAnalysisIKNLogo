d1e73c8d6ad12d7f53c52c3211ea3db2ea32f452
MKMaarif <maarif2910@students.undip.ac.id>
2023-06-29 00:31:23 +0700

    add prediction to py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
@@ -6,9 +8,30 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+# prediction function
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1)
+    model = pickle.load(open('model/sentiment_model.pkl', 'rb'))
+    tfidf = pickle.load(open('model/tfidf.pkl', 'rb'))
+    result = model.predict(tfidf.transform(to_predict))
+    return result[0]
+
+
 @app.route('/predict',methods=['POST'])
 def predict():
-    return render_template('index.html', prediction_text='The predicted value is 0.0')
+    if request.method == 'POST':
+        komen = request.form['komen']
+        
+        to_predict_list = komen
+        result = ValuePredictor(to_predict_list)
+        if result == 'positive':
+            prediction = 'Positive'
+        elif result == 'negative':
+            prediction = 'Negative'
+        else:
+            prediction = 'Neutral'
+
+    return prediction
 
 if __name__ == "__main__":
     app.run()
