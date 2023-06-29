b82268dfb72e3de3e0608412c98e9439b7906efa
farrel216 <farrel.arp@gmail.com>
2023-06-29 17:35:14 +0700

    preprocess

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# import library
+import re
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
 from flask import Flask,render_template,request
 import pickle
 import numpy as np
@@ -7,6 +14,32 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+def preprocess_text(text):
+    # remove url
+    text = re.sub(r"http\S+", " ", text)
+    # remove angka
+    text = re.sub(r"\d+", " ", text)
+    # remove punctuation
+    text = text.translate(str.maketrans(" "," ",string.punctuation))
+    # remove whitespace leading & trailing
+    text = text.strip()
+    # remove multiple whitespace into single whitespace
+    text = re.sub('\s+',' ',text)
+    # case folding
+    text = text.lower()
+    # tokenization
+    text = word_tokenize(text)
+    # remove stopwords
+    stop_words = set(stopwords.words('indonesian'))
+    text = [word for word in text if not word in stop_words]
+    # stemming
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+    text = [stemmer.stem(word) for word in text]
+    # join text
+    text = ' '.join(text)
+    return text
 
 # prediction function
 def ValuePredictor(to_predict_list):
@@ -21,7 +54,8 @@ def ValuePredictor(to_predict_list):
 def predict():
     if request.method == 'POST':
         komen = request.form['komen']
-        result = ValuePredictor(komen)
+        clean_komen = preprocess_text(komen)
+        result = ValuePredictor(clean_komen)
         if result == 'positive':
             prediction = 'Positive'
         elif result == 'negative':
